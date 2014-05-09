535e9d725f48254a6dd63708b69e9a65d4c1d04c
yume15 <g12915iy@gm.tsuda.ac.jp>
2014-05-09 12:08:57 +0900

    1000まで行ったよ

--- IntToEng/src/IntToEng.java
+++ IntToEng/src/IntToEng.java
@@ -12,11 +12,26 @@ public class IntToEng {
 	}
 	
 	static String translateEng(int n){
-		String[] a = {"zero","one","two","three","four","five","six","seven","eigjt","nine",
+		String[] a = {"zero","one","two","three","four","five","six","seven","eight","nine",
 				"ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eigthteen","nineteen"};
-		
-		String[] b = {" "," ","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety","one hundred"};
-		
-		return "";
+		String[] b = {" "," ","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","one hundred"};
+		String[] c = {"hundred ", "thousand "};		
+		String s;
+		if(n<20){return a[n];}
+		else if(20<=n && n<=100){
+			s = b[n/10];
+			if(n%10!=0) s+=a[n%10];
+			return s;
+		}
+		else if(100<n && n<1000){
+			s = a[n/100]+c[0];
+			if(n%100!=0){
+				n = n%100;
+				s+= b[n/10];
+			}
+			if(n%10!=0) s+= a[n%10];
+			return s;
+		}
+		else return "‚¿‚å‚Á‚Æ‘Ò‚Á‚Ä‚Ë";
 	}
 }
